d24476e770aae2157529b93ffa81e8eda9176ec8
Azizbek <azizbek@MacBook-Air-Olim.local>
2021-09-16 08:45:08 +0500

    first commit

---
+++ app/src/main/java/com/jeysoft/stories_library/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jeysoft.stories_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
